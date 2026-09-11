013f08cfafb9813620f356d5a6867b85badae7f8
Albert Esparragoza <57463296+albertesparragoza@users.noreply.github.com>
2026-09-11 13:28:52 +0000

    docs(examples): add a nuxt example

---
+++ examples/nuxt/nuxt.config.ts
@@ -0,0 +1,10 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2026-09-11',
+  telemetry: false,
+  ssr: true,
+  imports: {
+    autoImport: false,
+  },
+  components: false,
+  devtools: { enabled: false },
+})
